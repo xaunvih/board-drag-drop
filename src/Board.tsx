b1e4da7936cdd7a365342d4b28a33b1beab8cd45
vinhmai <vinhmai079@gmail.com>
2022-02-21 00:12:21 +0700

    separate grid component

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,40 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { useDebounce } from 'usehooks-ts'
-import { FixedSizeGrid, GridChildComponentProps } from 'react-window'
 import './Board.scss'
+import Grid from './Grid'
+import { useDebounce } from 'usehooks-ts'
 import { createZigZagArrFromN } from './utils'
 
 function Board(): React.ReactElement {
-    const [n, updateN] = useState<number>(5)
+    const [n, updateN] = useState<number>(10)
     const debouncedN = useDebounce<number>(n, 500)
     const additionalClass = debouncedN > 7 ? 'big' : 'small'
     const [zigZagArr, updateZigZagArr] = useState<number[][]>(createZigZagArrFromN(debouncedN))
 
-    function onDragStart(evt: React.DragEvent<HTMLDivElement>, rowIndex: number, columnIndex: number) {
-        evt.dataTransfer.setData('text/plain', JSON.stringify({ rowIndex, columnIndex }))
-    }
-
-    function onDragOver(evt: React.DragEvent<HTMLDivElement>) {
-        evt.preventDefault()
-    }
-
-    function onDrop(evt: React.DragEvent<HTMLDivElement>, rowIndex: number, columnIndex: number) {
-        // check itself
-
-        const data = JSON.parse(evt.dataTransfer.getData('text/plain')) as { rowIndex: number; columnIndex: number }
-        const newZigZagArr = [...zigZagArr]
-
-        const { rowIndex: sourceRowIndex, columnIndex: sourceColumnIndex } = data
-        const current = newZigZagArr[sourceRowIndex][sourceColumnIndex]
-
-        newZigZagArr[data.rowIndex][data.columnIndex] = newZigZagArr[rowIndex][columnIndex]
-        newZigZagArr[rowIndex][columnIndex] = current
-        updateZigZagArr(newZigZagArr)
-    }
-
     function onNumberChange(evt: React.ChangeEvent<HTMLInputElement>) {
         const number = evt.target.value
-        updateN(Number(number))
+        if (number) {
+            updateN(Number(number))
+        }
     }
 
     useEffect(() => {
@@ -42,46 +22,17 @@ function Board(): React.ReactElement {
         updateZigZagArr(newZigZagArr)
     }, [debouncedN])
 
-    function Cell(props: GridChildComponentProps) {
-        const { columnIndex, rowIndex, style } = props
-        const number = zigZagArr[rowIndex][columnIndex]
-
-        return (
-            <div
-                onDragStart={(evt) => onDragStart(evt, rowIndex, columnIndex)}
-                onDragOver={onDragOver}
-                onDrop={(evt) => onDrop(evt, rowIndex, columnIndex)}
-                draggable={true}
-                className="cell"
-                style={style}
-                key={number}
-            >
-                {number}
-            </div>
-        )
-    }
-
     return (
-        <div className="container">
+        <main className="container">
             <header className="header">
                 <h2>Type number of row and column:</h2>
-                <input type="number" defaultValue={debouncedN} onChange={onNumberChange} min={1} max={1000} />
+                <input type="number" defaultValue={debouncedN} onChange={onNumberChange} min={1} />
             </header>
 
             <div className={`board ${additionalClass}`}>
-                <FixedSizeGrid
-                    columnCount={debouncedN}
-                    rowCount={debouncedN}
-                    columnWidth={100}
-                    rowHeight={100}
-                    height={700}
-                    width={700}
-                    className="grid"
-                >
-                    {Cell}
-                </FixedSizeGrid>
+                <Grid zigZagArr={zigZagArr} updateZigZagArr={updateZigZagArr} />
             </div>
-        </div>
+        </main>
     )
 }
 
---
+++ src/Grid.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { FixedSizeGrid, GridChildComponentProps } from 'react-window'
+import './Board.scss'
+
+interface GridProps {
+    zigZagArr: number[][]
+    updateZigZagArr: React.Dispatch<React.SetStateAction<number[][]>>
+}
+
+function Grid(props: GridProps): React.ReactElement {
+    const { zigZagArr, updateZigZagArr } = props
+
+    function onDragStart(evt: React.DragEvent<HTMLDivElement>, rowIndex: number, columnIndex: number) {
+        evt.dataTransfer.setData('text/plain', JSON.stringify({ rowIndex, columnIndex }))
+    }
+
+    function onDragOver(evt: React.DragEvent<HTMLDivElement>) {
+        evt.preventDefault()
+    }
+
+    function onDrop(evt: React.DragEvent<HTMLDivElement>, rowIndex: number, columnIndex: number) {
+        const data = JSON.parse(evt.dataTransfer.getData('text/plain')) as { rowIndex: number; columnIndex: number }
+        const newZigZagArr = [...zigZagArr]
+        const { rowIndex: sourceRowIndex, columnIndex: sourceColumnIndex } = data
+        const current = newZigZagArr[sourceRowIndex][sourceColumnIndex]
+        const target = newZigZagArr[rowIndex][columnIndex]
+
+        if (current === target) {
+            // drop on same square
+            return
+        }
+
+        newZigZagArr[sourceRowIndex][sourceColumnIndex] = target
+        newZigZagArr[rowIndex][columnIndex] = current
+        updateZigZagArr(newZigZagArr)
+    }
+
+    function Cell(props: GridChildComponentProps) {
+        const { columnIndex, rowIndex, style } = props
+        const number = zigZagArr[rowIndex][columnIndex]
+
+        return (
+            <div
+                onDragStart={(evt) => onDragStart(evt, rowIndex, columnIndex)}
+                onDragOver={onDragOver}
+                onDrop={(evt) => onDrop(evt, rowIndex, columnIndex)}
+                draggable={true}
+                className="cell"
+                style={style}
+                key={number}
+            >
+                {number}
+            </div>
+        )
+    }
+
+    return (
+        <FixedSizeGrid
+            columnCount={zigZagArr[0].length}
+            rowCount={zigZagArr[0].length}
+            columnWidth={100}
+            rowHeight={100}
+            height={700}
+            width={700}
+            className="grid"
+        >
+            {Cell}
+        </FixedSizeGrid>
+    )
+}
+
+export default Grid
